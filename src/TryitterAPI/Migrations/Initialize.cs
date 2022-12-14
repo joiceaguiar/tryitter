89a0897034f73a4616594d694e89fef0e2aff426
Joice <joicemdsaguiar@gmail.com>
2022-12-14 14:53:19 -0300

    :sparkles: feat: added initialize migrations

---
+++ src/TryitterAPI/Migrations/Initialize.cs
@@ -0,0 +1,56 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TryitterAPI.Migrations
+{
+    public partial class Initialize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Post_Students_StudentId",
+                table: "Post");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StudentId",
+                table: "Post",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Post_Students_StudentId",
+                table: "Post",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Post_Students_StudentId",
+                table: "Post");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StudentId",
+                table: "Post",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Post_Students_StudentId",
+                table: "Post",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
